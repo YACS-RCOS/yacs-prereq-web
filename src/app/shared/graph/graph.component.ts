cce1b8dd726a548495dd2e7333c7b24aa9c72298
rystills <rystills@gmail.com>
2017-06-02 16:21:25 -0400

    fixed formatting, create spaced nodes from dict

--- src/app/shared/graph/graph.component.ts
+++ src/app/shared/graph/graph.component.ts
@@ -1,110 +1,126 @@
-import { Component, OnInit, OnChanges, ViewChild, ElementRef, Input, ViewEncapsulation } from '@angular/core';
+import {
+	Component,
+	OnInit,
+	OnChanges,
+	ViewChild,
+	ElementRef,
+	Input,
+	ViewEncapsulation
+} from '@angular/core';
 import * as d3 from 'd3';
 
 @Component({
-  selector: 'app-graph',
-  templateUrl: './graph.component.html',
-  styleUrls: ['./graph.component.css']
+	selector: 'app-graph',
+	templateUrl: './graph.component.html',
+	styleUrls: ['./graph.component.css']
 })
 
 export class GraphComponent implements OnInit {
 
-  @Input() private data: Array<any>;
-  @ViewChild('graph') private graphContainer: ElementRef;
-  private margin: any = { top: 20, bottom: 20, left: 20, right: 20};
-  private graph: any;
-  private nodeRadius: number;
-  private userCanEdit: boolean;
-  private colors: any;
-  private strokeWidth : number;
+	@Input() private data: Array < any > ;
+	@ViewChild('graph') private graphContainer: ElementRef;
+	//private margin: any = { top: 20, bottom: 20, left: 20, right: 20};
+	private graph: any;
+	private nodeRadius: number;
+	private userCanEdit: boolean;
+	private colors: any;
+	private strokeWidth: number;
+	private nodeSpacing: number;
 
-  constructor() { 
+	constructor() {
 
-  }
+	}
 
-  ngOnInit() {
-  this.createGraph();
-    if (this.data) {
-      this.updateGraph();
-    }
-  }
-  
-  ngOnChanges() {
-    if (this.graph) {
-      this.updateGraph();
-    }
-  }
-	
-  createGraph() {
-    var width = window.innerWidth;
-    var height = window.innerHeight;
+	ngOnInit() {
+		this.createGraph();
+		if (this.data) {
+			this.updateGraph();
+		}
+	}
 
-    let element = this.graphContainer.nativeElement;
-    this.graph = d3.select(element).append('svg')
-      .attr('width', width)
-      .attr('height', height);
+	ngOnChanges() {
+		if (this.graph) {
+			this.updateGraph();
+		}
+	}
 
-    // init constants
-    this.nodeRadius = 40;
-    this.userCanEdit = false;
-    this.strokeWidth = 3;
+	createGraph() {
+		var width = window.innerWidth;
+		var height = window.innerHeight;
 
-    //Draw a circle to verify that we are executing correctly
-    console.log(width);
-    console.log(height);
+		let element = this.graphContainer.nativeElement;
+		this.graph = d3.select(element).append('svg')
+			.attr('width', width)
+			.attr('height', height);
 
-  var circle = this.graph.append("svg")
-  .attr("x",width/2)
-  .attr("y",height/2)
-  .attr("width",this.nodeRadius*2+this.strokeWidth*2)
-  .attr("height",this.nodeRadius*2+this.strokeWidth*2);
+		// init constants
+		this.nodeRadius = 40;
+		this.userCanEdit = false;
+		this.strokeWidth = 3;
+		this.nodeSpacing = 8;
 
-  var circleGraphic = circle.append("circle")
-                         .attr("cx", this.nodeRadius+this.strokeWidth)
-                         .attr("cy", this.nodeRadius+this.strokeWidth)
-                         .attr("r", this.nodeRadius)
-                         .attr("stroke","black")
-                         .attr("stroke-width",this.strokeWidth)
-                         .attr("fill","red");
-var circleText = circle.append("text") 
-.attr("x",this.nodeRadius+this.strokeWidth)
-.attr("y",this.nodeRadius+this.strokeWidth)
-.attr("font-size","20px")
-.attr("text-anchor", "middle")
-.attr("alignment-baseline", "central")
-.text("1100")
-  }
+		var curX: number = width / 2;
+		var curY: number = height / 2;
 
-  updateGraph() {
-    /*
-    let update = this.graph.selectAll('.bar')
-      .data(this.data);
+		//draw graph nodes
+		var nodeNames: string[] = ["1100", "1200"]
+		for (let name of nodeNames) {
+			var circle = this.graph.append("svg")
+				.attr("x", curX)
+				.attr("y", curY)
+				.attr("width", this.nodeRadius * 2 + this.strokeWidth * 2)
+				.attr("height", this.nodeRadius * 2 + this.strokeWidth * 2);
 
-    // remove exiting bars
-    update.exit().remove();
+			var circleGraphic = circle.append("circle")
+				.attr("cx", this.nodeRadius + this.strokeWidth)
+				.attr("cy", this.nodeRadius + this.strokeWidth)
+				.attr("r", this.nodeRadius)
+				.attr("stroke", "black")
+				.attr("stroke-width", this.strokeWidth)
+				.attr("fill", "red");
 
-    // update existing bars
-    this.graph.selectAll('.bar').transition()
-      .attr('x', d => 20
-      .attr('y', d => 30
-      .attr('width', d => this.nodeRadius
-      .attr('height', d => this.nodeRadius
-      .style('fill', (d, i) => this.colors(i));
+			var circleText = circle.append("text")
+				.attr("x", this.nodeRadius + this.strokeWidth)
+				.attr("y", this.nodeRadius + this.strokeWidth)
+				.attr("font-size", "20px")
+				.attr("text-anchor", "middle")
+				.attr("alignment-baseline", "central")
+				.text(name)
 
-    // add new bars
-    update
-      .enter()
-      .append('rect')
-      .attr('class', 'bar')
-      .attr('x', d => 20
-      .attr('y', d => 30
-      .attr('width', this.nodeRadius
-      .attr('height', 0)
-      .style('fill', (d, i) => this.colors(i))
-      .transition()
-      .delay((d, i) => i * 10)
-      .attr('y', d => 30);
-      .attr('height', d => this.nodeRadius);
-  	*/
-  }
+			curY += (this.nodeRadius + this.strokeWidth) * 2 + this.nodeSpacing;
+		}
+	}
+
+	updateGraph() {
+		/*
+		let update = this.graph.selectAll('.bar')
+			.data(this.data);
+
+		// remove exiting bars
+		update.exit().remove();
+
+		// update existing bars
+		this.graph.selectAll('.bar').transition()
+			.attr('x', d => 20
+			.attr('y', d => 30
+			.attr('width', d => this.nodeRadius
+			.attr('height', d => this.nodeRadius
+			.style('fill', (d, i) => this.colors(i));
+
+		// add new bars
+		update
+			.enter()
+			.append('rect')
+			.attr('class', 'bar')
+			.attr('x', d => 20
+			.attr('y', d => 30
+			.attr('width', this.nodeRadius
+			.attr('height', 0)
+			.style('fill', (d, i) => this.colors(i))
+			.transition()
+			.delay((d, i) => i * 10)
+			.attr('y', d => 30);
+			.attr('height', d => this.nodeRadius);
+		*/
+	}
 }
