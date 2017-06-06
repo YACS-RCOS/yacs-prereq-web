93426539c643d8d84215be30a38a64798d4281ab
rystills <rystills@gmail.com>
2017-06-06 16:58:45 -0400

    move prereq data file to public 'assets' folder, read nodes from json

--- src/app/shared/graph/graph.component.ts
+++ src/app/shared/graph/graph.component.ts
@@ -53,6 +53,9 @@ export class GraphComponent implements OnInit {
 	}
 
 	createGraph() {
+		//maintain a reference to this in case we need to access it while inside a selection
+		let baseThis = this;
+
 		let width = window.innerWidth;
 		let height = window.innerHeight;
 
@@ -62,151 +65,166 @@ export class GraphComponent implements OnInit {
 			.attr('height', height);
 
 		// init constants
-		this.nodeRadius = 40;
+		this.nodeRadius = 50;
 		this.userCanEdit = false;
 		this.strokeThickness = 2;
 		this.edgeThickness = 4;
 		this.nodeSpacing = 24;
 
 		let curX: number = width / 2;
-		let curY: number = height / 2;
+		let curY: number = this.nodeSpacing;
 
 		//hard-code nodes and edges until we reimplement loading prereqs from JSON
 		type edge = {startNode: string, endNode: string}
-		let nodeNames: string[] = ["CS1100", "CS1200", "CS2100"]
-		let edges : edge[] = [({startNode:"CS1100",endNode:"CS1200"}),({startNode:"CS1200",endNode:"CS2100"})]
 		
-		//dictionary of 'name' : 'svg parent' for easy node access
-		this.nodeDict = {};
-		//dictionary of 'startNodeName' : 'svg parent' and 'endNodeName' : 'svg parent' for easy edge access
-		this.edgeDict = {};
+		/*let nodeNames: string[] = ["CSCI 1100", "CSCI 1200", "CSCI 2100"]*/
+		let edges : edge[] = [({startNode:"CSCI 1100",endNode:"CSCI 1200"}),({startNode:"CSCI 1200",endNode:"CSCI 2100"})]
 
-		//maintain a reference to this in case we need to access it while inside a selection
-		let baseThis = this;
+		let nodeData;
+		let metaNodeData;
+		d3.json("/assets/prereq_data.json", function(prereqs) {
+			nodeData = prereqs["CSCI_nodes"];
+			metaNodeData = prereqs["meta_nodes"];
+			//dictionary of 'name' : 'svg parent' for easy node access
+			baseThis.nodeDict = {};
+			//dictionary of 'name' : 'list of connected edges' for easy edge access
+			baseThis.edgeDict = {};
 
-		//construct edge and node group parents
-		let edgeParent = this.graph.append("g").attr("id","edges");
-		let nodeParent = this.graph.append("g").attr("id","nodes");
+			for (let node of nodeData) {
+				console.log(node.course_uid);
+			}
 
-		//setup mouse events on graph
-		this.graph.on("mousedown", function() {
-			//poll for closest node on mouseDown
-			baseThis.startCoords = d3.mouse(this);
-			let closestNode = null;
-			let closestDistance = -1;
-			for (let nodeKey in baseThis.nodeDict) {
-				let curCircle = baseThis.nodeDict[nodeKey];
-				let curDistance = Math.sqrt(
-					Math.pow(+curCircle.attr("x") + +curCircle.attr("width") / 2 - baseThis.startCoords[0], 2) +
-					Math.pow(+curCircle.attr("y") + +curCircle.attr("height") / 2 - baseThis.startCoords[1], 2));
-				if (closestDistance == -1 || curDistance < closestDistance) {
-					closestDistance = curDistance;
-					closestNode = baseThis.nodeDict[nodeKey];
+			//construct edge and node group parents
+			let edgeParent = baseThis.graph.append("g").attr("id","edges");
+			let nodeParent = baseThis.graph.append("g").attr("id","nodes");
+
+			//setup mouse events on graph
+			baseThis.graph.on("mousedown", function() {
+				//poll for closest node on mouseDown
+				baseThis.startCoords = d3.mouse(this);
+				let closestNode = null;
+				let closestDistance = -1;
+				for (let nodeKey in baseThis.nodeDict) {
+					let curCircle = baseThis.nodeDict[nodeKey];
+					//distance formula
+					let curDistance = Math.sqrt(
+						Math.pow(+curCircle.attr("x") + +curCircle.attr("width") / 2 - baseThis.startCoords[0], 2) +
+						Math.pow(+curCircle.attr("y") + +curCircle.attr("height") / 2 - baseThis.startCoords[1], 2));
+					if (closestDistance == -1 || curDistance < closestDistance) {
+						closestDistance = curDistance;
+						closestNode = baseThis.nodeDict[nodeKey];
+					}
 				}
-			}
 
-			//check if closest node is within drag start distance
-			if (closestDistance <= baseThis.nodeRadius) {
-				baseThis.dragging = true;
-				baseThis.dragNode = closestNode;
-			}
-			
-		})
-		.on("mousemove", function() {
-			//check for node dragging
-			if (baseThis.dragging) {
-				let coords = d3.mouse(this);
-	            let dx = (coords[0] - baseThis.startCoords[0]);
-	            let dy = (coords[1] - baseThis.startCoords[1]);
-	            console.log(dx)
-	            //note the use of the unary operator '+' to convert string attr to int (the first '+' is NOT a concatenation)
-	            baseThis.dragNode.attr("x", +baseThis.dragNode.attr("x") + dx);
-	        	baseThis.dragNode.attr("y", +baseThis.dragNode.attr("y") + dy);
-
-	        	//move all connected edges depending on whether we are the startNode or the endNode of that edge
-	        	let connectedEdges = baseThis.edgeDict[baseThis.dragNode.attr("id")];
-	        	for (let curEdge of connectedEdges) {
-	        		baseThis.recalculateEdge(curEdge);
-	        	}
-	        	baseThis.startCoords[0] += dx;
-	        	baseThis.startCoords[1] += dy;
-			}
-		})
-
-		.on("mouseup", function() {
-			baseThis.dragging = false;
-		});
-
-		//construct graph nodes
-		for (let name of nodeNames) {
-			//node parent
-			let circle = nodeParent.append("svg")
-				.attr("x", curX)
-				.attr("y", curY)
-				.attr("width", this.nodeRadius * 2 + this.strokeThickness * 2)
-				.attr("height", this.nodeRadius * 2 + this.strokeThickness * 2)
-				.attr("isDown",true)
-				.attr("id",name)  
-
-			//node circle element
-			let circleGraphic = circle.append("circle")
-				.attr("cx", this.nodeRadius + this.strokeThickness)
-				.attr("cy", this.nodeRadius + this.strokeThickness)
-				.attr("r", this.nodeRadius)
-				.attr("stroke", "black")
-				.attr("stroke-width", this.strokeThickness)
-				.attr("fill", "rgb(200,200,255)");
-
-			//node text element
-			let circleText = circle.append("text")
-				.attr("class", "svgtxt")
-				.attr("x", this.nodeRadius + this.strokeThickness)
-				.attr("y", this.nodeRadius + this.strokeThickness)
-				.attr("font-size", "20px")
-				.attr("text-anchor", "middle")
-				.attr("alignment-baseline", "central")
-				.text(name);
+				//check if closest node is within drag start distance
+				if (closestDistance <= baseThis.nodeRadius) {
+					baseThis.dragging = true;
+					baseThis.dragNode = closestNode;
+				}
 				
+			})
+			.on("mousemove", function() {
+				//check for node dragging
+				if (baseThis.dragging) {
+					let coords = d3.mouse(this);
+		            let dx = (coords[0] - baseThis.startCoords[0]);
+		            let dy = (coords[1] - baseThis.startCoords[1]);
+		            console.log(dx)
+		            //note the use of the unary operator '+' to convert string attr to int (the first '+' is NOT a concatenation)
+		            baseThis.dragNode.attr("x", +baseThis.dragNode.attr("x") + dx);
+		        	baseThis.dragNode.attr("y", +baseThis.dragNode.attr("y") + dy);
 
-			this.nodeDict[name] = circle;
-			curY += (this.nodeRadius + this.strokeThickness) * 2 + this.nodeSpacing;
-			curX += 50;
-		}
+		        	//move all connected edges depending on whether we are the startNode or the endNode of that edge
+		        	let connectedEdges = baseThis.edgeDict[baseThis.dragNode.attr("id")];
+		        	if (connectedEdges) {
+		        		for (let curEdge of connectedEdges) {
+		        			baseThis.recalculateEdge(curEdge);
+		        		}
+		        	}
+		        	baseThis.startCoords[0] += dx;
+			        baseThis.startCoords[1] += dy;
+				}
+			})
 
-		//construct graph edges
-		for (let edge of edges) {
-			let startNode = this.nodeDict[edge.startNode];
-			let endNode = this.nodeDict[edge.endNode];
+			.on("mouseup", function() {
+				baseThis.dragging = false;
+			});
 
-			let startNodeX = +startNode.attr("x") + +startNode.attr("width") /2;
-			let startNodeY = +startNode.attr("y") + +startNode.attr("height") /2;
-			let endNodeX = +endNode.attr("x") + +endNode.attr("width") /2;
-			let endNodeY = +endNode.attr("y") + +endNode.attr("height") /2;
-			
-			let edgeWidth = Math.abs(startNodeX - endNodeX);
-			let edgeHeight = Math.abs(startNodeY - endNodeY);
-			
-			//edge parent
-			let newEdge = edgeParent.append("svg")
-			.attr("startNodeID", startNode.attr("id"))
-			.attr("endNodeID", endNode.attr("id"));
+			//construct graph nodes
+			for (let node of nodeData) {
+				//node parent
+				let circle = nodeParent.append("svg")
+					.attr("x", curX)
+					.attr("y", curY)
+					.attr("width", baseThis.nodeRadius * 2 + baseThis.strokeThickness * 2)
+					.attr("height", baseThis.nodeRadius * 2 + baseThis.strokeThickness * 2)
+					.attr("isDown",true)
+					.attr("id",node.course_uid)  
 
-			//edge line element
-			let edgeGraphic = newEdge.append("line")
-			.attr("stroke-width",this.edgeThickness)
-			.attr("stroke", "black");
+				//node circle element
+				let circleGraphic = circle.append("circle")
+					.attr("cx", baseThis.nodeRadius + baseThis.strokeThickness)
+					.attr("cy", baseThis.nodeRadius + baseThis.strokeThickness)
+					.attr("r", baseThis.nodeRadius)
+					.attr("stroke", "black")
+					.attr("stroke-width", baseThis.strokeThickness)
+					.attr("fill", "rgb(200,200,255)");
 
-			if (!this.edgeDict[edge.startNode]) {
-				this.edgeDict[edge.startNode] = [];
+				//node text element
+				let circleText = circle.append("text")
+					.attr("class", "svgtxt")
+					.attr("x", baseThis.nodeRadius + baseThis.strokeThickness)
+					.attr("y", baseThis.nodeRadius + baseThis.strokeThickness)
+					.attr("font-size", "20px")
+					.attr("text-anchor", "middle")
+					.attr("alignment-baseline", "central")
+					.text(node.course_uid);
+					
+
+				baseThis.nodeDict[node.course_uid] = circle;
+				curY += (baseThis.nodeRadius + baseThis.strokeThickness) * 2 + baseThis.nodeSpacing;
+				curX += 50;
 			}
-			this.edgeDict[edge.startNode].push(newEdge);
-			if (!this.edgeDict[edge.endNode]) {
-				this.edgeDict[edge.endNode] = [];
-			}
-			this.edgeDict[edge.endNode].push(newEdge);
 
-			this.recalculateEdge(newEdge);
-		}
+			//construct graph edges
+			for (let edge of edges) {
+				let startNode = baseThis.nodeDict[edge.startNode];
+				let endNode = baseThis.nodeDict[edge.endNode];
+
+				if (! (startNode && endNode)) {
+					continue;
+				}
+
+				let startNodeX = +startNode.attr("x") + +startNode.attr("width") /2;
+				let startNodeY = +startNode.attr("y") + +startNode.attr("height") /2;
+				let endNodeX = +endNode.attr("x") + +endNode.attr("width") /2;
+				let endNodeY = +endNode.attr("y") + +endNode.attr("height") /2;
+				
+				let edgeWidth = Math.abs(startNodeX - endNodeX);
+				let edgeHeight = Math.abs(startNodeY - endNodeY);
+				
+				//edge parent
+				let newEdge = edgeParent.append("svg")
+				.attr("startNodeID", startNode.attr("id"))
+				.attr("endNodeID", endNode.attr("id"));
+
+				//edge line element
+				let edgeGraphic = newEdge.append("line")
+				.attr("stroke-width",baseThis.edgeThickness)
+				.attr("stroke", "black");
+
+				if (!baseThis.edgeDict[edge.startNode]) {
+					baseThis.edgeDict[edge.startNode] = [];
+				}
+				baseThis.edgeDict[edge.startNode].push(newEdge);
+				if (!baseThis.edgeDict[edge.endNode]) {
+					baseThis.edgeDict[edge.endNode] = [];
+				}
+				baseThis.edgeDict[edge.endNode].push(newEdge);
+
+				baseThis.recalculateEdge(newEdge);
+			}
+		});
 	}
 
 	/*recalculate position, dimensions, and line position of connectedEdge*/
@@ -251,16 +269,6 @@ export class GraphComponent implements OnInit {
 			edgeLine.attr("y1",edgeHeight + this.edgeThickness/ 2)
 			edgeLine.attr("y2", this.edgeThickness / 2);
 		}
-
-
-
-		//load in course data
-		/*var req = new XMLHttpRequest();
-		req.open('GET', '/prereq_data.json');
-		req.onreadystatechange = function() {
-			console.log("LOADED PREREQ DATA");
-		}
-		req.send();*/
 	}
 
 	updateGraph() {
